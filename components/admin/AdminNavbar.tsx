cea6c33086e2daa15db6e66683dd51a4782416a3
Fernando Pons <fernando.pons@estoes.me>
2023-04-03 18:58:17 -0300

    fixed interfaces

--- components/admin/AdminNavbar.tsx
+++ components/admin/AdminNavbar.tsx
@@ -1,34 +1,27 @@
-import { useContext } from 'react';
-import NextLink from 'next/link';
+import { useContext } from "react"
+import NextLink from "next/link"
 
+import { AppBar, Box, Button, Link, Toolbar, Typography } from "@mui/material"
 
-import { AppBar, Box, Button, Link, Toolbar, Typography } from '@mui/material';
-
-
-import { UiContext } from '../../context';
+import { UiContext } from "@/context"
 
 export const AdminNavbar = () => {
+  const { toogleSideMenu } = useContext(UiContext)
 
-    const { toggleSideMenu } = useContext( UiContext );
-    
+  return (
+    <AppBar>
+      <Toolbar>
+        <NextLink href="/" passHref>
+          <Link display="flex" alignItems="center">
+            <Typography variant="h6">Teslo |</Typography>
+            <Typography sx={{ ml: 0.5 }}>Shop</Typography>
+          </Link>
+        </NextLink>
 
-    return (
-        <AppBar>
-            <Toolbar>
-                <NextLink href='/' passHref>
-                    <Link display='flex' alignItems='center'>
-                        <Typography variant='h6'>Teslo |</Typography>
-                        <Typography sx={{ ml: 0.5 }}>Shop</Typography>
-                    </Link>  
-                </NextLink>
+        <Box flex={1} />
 
-                <Box flex={ 1 } />
-
-                <Button onClick={ toggleSideMenu }>
-                    Menú
-                </Button>
-
-            </Toolbar>
-        </AppBar>
-    )
+        <Button onClick={toogleSideMenu}>Menú</Button>
+      </Toolbar>
+    </AppBar>
+  )
 }
--- components/layouts/AdminLayout.tsx
+++ components/layouts/AdminLayout.tsx
@@ -1,50 +1,42 @@
-import { FC } from 'react';
-import { Box, Typography } from '@mui/material';
-import { AdminNavbar } from '../admin';
+import { FC, PropsWithChildren } from "react"
+import { Box, Typography } from "@mui/material"
+import { AdminNavbar } from "../admin"
 
-import { SideMenu } from '../ui';
+import { SideMenu } from "../ui"
 
-
-interface Props {
-    title: string;
-    subTitle: string;
-    icon?: JSX.Element;
+interface Props extends PropsWithChildren {
+  title: string
+  subTitle: string
+  icon?: JSX.Element
 }
 
-export const AdminLayout:FC<Props> = ({ children, title, subTitle, icon }) => {
+export const AdminLayout: FC<Props> = ({ children, title, subTitle, icon }) => {
   return (
     <>
+      <nav>
+        <AdminNavbar />
+      </nav>
 
-        <nav>
-            <AdminNavbar />
-        </nav>
+      <SideMenu />
 
-        <SideMenu />
+      <main
+        style={{
+          margin: "80px auto",
+          maxWidth: "1440px",
+          padding: "0px 30px",
+        }}
+      >
+        <Box display="flex" flexDirection="column">
+          <Typography variant="h1" component="h1">
+            {icon} {title}
+          </Typography>
+          <Typography variant="h2" sx={{ mb: 1 }}>
+            {subTitle}
+          </Typography>
+        </Box>
 
-        <main style={{
-            margin: '80px auto',
-            maxWidth: '1440px',
-            padding: '0px 30px'
-        }}>
-
-            <Box display="flex" flexDirection='column'>
-                <Typography variant='h1' component='h1'>
-                    { icon }
-                    {' '} { title }
-                </Typography>
-                <Typography variant='h2' sx={{ mb: 1 }}>{ subTitle }</Typography>
-
-            </Box>
-
-            <Box className='fadeIn'>
-                { children }
-            </Box>
-
-        </main>
-
-
+        <Box className="fadeIn">{children}</Box>
+      </main>
     </>
   )
 }
-
-
