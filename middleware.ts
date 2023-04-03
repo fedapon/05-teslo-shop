91a4ab6dedd5a30e255a67e5ebff367b7a8deb8d
Fernando Pons <fernando.pons@estoes.me>
2023-04-03 18:33:56 -0300

    fixed middleware protection

--- middleware.ts
+++ middleware.ts
@@ -3,19 +3,67 @@ import { getSession } from "next-auth/react"
 import { NextFetchEvent, NextRequest, NextResponse } from "next/server"
 
 export async function middleware(req: NextRequest) {
-    const session = await getToken({ req, secret: process.env.NEXTAUTH_SECRET })
+  const session: any = await getToken({
+    req,
+    secret: process.env.NEXTAUTH_SECRET,
+  })
 
+  if (req.nextUrl.pathname.startsWith("/api/admin")) {
     if (!session) {
-        const requestedPage = req.nextUrl.pathname
-        const url = req.nextUrl.clone()
-        url.pathname = "/auth/login"
-        url.search = `p=${requestedPage}`
-        return NextResponse.redirect(url)
+      return new Response(JSON.stringify({ message: "No autorizado" }), {
+        status: 401,
+        headers: {
+          "Content-Type": "application/json",
+        },
+      })
+    }
+
+    const validRoles = ["admin", "super-user", "SEO"]
+    if (!validRoles.includes(session.user.role)) {
+      return new Response(JSON.stringify({ message: "No autorizado" }), {
+        status: 401,
+        headers: {
+          "Content-Type": "application/json",
+        },
+      })
     }
 
     return NextResponse.next()
+  }
+
+  if (req.nextUrl.pathname.startsWith("/admin")) {
+    if (!session) {
+      const requestedPage = req.nextUrl.pathname
+      return NextResponse.redirect(`/auth/login?p=${requestedPage}`)
+    }
+
+    const validRoles = ["admin", "super-user", "SEO"]
+
+    if (!validRoles.includes(session.user.role)) {
+      const url = req.nextUrl.clone()
+      url.pathname = "/"
+      return NextResponse.redirect(url)
+    }
+
+    return NextResponse.next()
+  }
+
+  if (!session) {
+    const requestedPage = req.nextUrl.pathname
+    const url = req.nextUrl.clone()
+    url.pathname = "/auth/login"
+    url.search = `p=${requestedPage}`
+    return NextResponse.redirect(url)
+  }
+
+  return NextResponse.next()
 }
 
 export const config = {
-    matcher: ["/checkout/address", "/checkout/summary"],
+  matcher: [
+    "/api/admin/:path*",
+    "/admin/:path*",
+    "/checkout/address",
+    "/checkout/summary",
+  ],
 }
